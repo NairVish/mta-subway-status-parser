89408f7b37be5a518aa0427e36a2cc3e20fb1fcc
Vishnu Nair <vishnu4nair@gmail.com>
2017-09-03 11:27:19 -0400

    Comments!

--- main.py
+++ main.py
@@ -1,17 +1,29 @@
 import requests
 import re
 from bs4 import BeautifulSoup
-# need lxml
+# also need lxml
 
 
 class SubwayStatusHandler:
-    STATUS_URL = "http://web.mta.info/status/serviceStatus.txt"
-    LINES = ["123", "456", "7", "ACE", "BDFM", "G", "JZ", "NQR", "S", "SIR"]
+    """
+    Handles retrieval, parsing, and return of subway data.
+    """
+    STATUS_URL = "http://web.mta.info/status/serviceStatus.txt" # the URL we're getting the data from
+    # LINES = ["123", "456", "7", "ACE", "BDFM", "G", "JZ", "NQR", "S", "SIR"]
 
     def __init__(self):
+        """
+        New data is automatically retrieved upon initialization of the object.
+        """
         self.load_new_data()
 
     def load_new_data(self):
+        """
+        Gets the new data, processes it, and assigns it to a member variable called "status_data".
+        We're only concerned with the Subway status portion of the data here.
+        (However, the program can easily be extended to include buses, bridges & tunnels, LIRR, etc.)
+        :return: Nothing.
+        """
         r = requests.get(self.STATUS_URL)
         raw_data = self._received_data_processor(r.text)
         soup = BeautifulSoup(raw_data, 'lxml')
@@ -19,6 +31,11 @@ class SubwayStatusHandler:
 
     @staticmethod
     def _received_data_processor(inp_data):
+        """
+        [Private] Replaces HTML character entities and other references with their appropriate characters.
+        :param inp_data: The input data we must sweep.
+        :return: The swept/cleaned up data.
+        """
         # replace html escape characters in first pass-through
         esc_reps = {"&amp;nbsp;": " ", "&lt;": "<", "&gt;": ">", "&nbsp;": " ", "&amp;": "&", "Â": ""}
         esc_reps = dict((re.escape(k), v) for k, v in esc_reps.items()) # escape everything except except ASCII letters, numbers and '_'.
@@ -37,6 +54,10 @@ class SubwayStatusHandler:
         return out_data
 
     def get_lines_with_text(self):
+        """
+        Gets all of the status data and packages it for return.
+        :return: Dict of the form: {"LINE_GROUP_NAME": (STATUS, STATUS_DESCRIPTION), ...}
+        """
         r = {}
         for line in self.status_data:
             line_name = line.find("name").getText()
@@ -45,8 +66,9 @@ class SubwayStatusHandler:
             r[line_name] = (line_status, status_description)
         return r
 
+# Some code to output the result.
 subway_line_data = SubwayStatusHandler().get_lines_with_text()
-print(subway_line_data)
+# print(subway_line_data)
 for l, s in subway_line_data.items():
     print(l + " - " + s[0])
     print(s[1])
